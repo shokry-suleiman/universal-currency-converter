feae0b2998e71bc8e5f2f92cc7937c012d2220ab
Shoukry <shokrysuleiman032@gmail.com>
2023-06-19 20:52:47 +0300

    feat(feat): :lipstick: Finish Responsive Design

--- src/components/currency-card.js
+++ src/components/currency-card.js
@@ -2,14 +2,14 @@ import unitedState from "../assets/icons/united-states-of-america.png";
 
 function CurrencyCard() {
     return (
-        <div className="flex items-center shadow-[0_1px_4px_0_rgba(0,0,0,0.25)] rounded-[7px] p-[36px] mb-[20px]">
-        <div className="text-black mr-8">USD</div>
+        <div className="flex items-center shadow-[0_1px_4px_0_rgba(0,0,0,0.25)] rounded-[7px] p-[24px] md:p-[36px] mb-[22px] lg:mb-[20px]">
+        <div className="text-black mr-6 md:mr-8 text-[21px]">USD</div>
         <img
-          className="border border-primary rounded-full w-[75px]"
+          className="border border-primary rounded-full w-[62px] md:w-[75px]"
           src={unitedState}
           alt="..."
         />
-        <div className="text-black  ml-auto  text-[16px]">0.38</div>
+        <div className="text-black  ml-auto  text-[21px]">0.38</div>
       </div>
     );
   }
--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -2,14 +2,14 @@ import euro from "../../assets/icons/euro.svg";
 import CurrencyCard from '../../components/currency-card.js'
 function Home() {
   return (
-    <div className="columns-2 w-screen h-screen gap-0">
-      <div className="h-full bg-primary flex flex-col justify-start items-center">
-        <img className="block mb-8 mt-[80px]" src={euro} alt="..." />
-        <input id="number" type="number" value={23.50}  className="bg-primary	text-center text-white font-bold text-[80px] focus:outline-none"/>  
+    <div className="lg:columns-2 w-screen h-screen gap-0">
+      <div className="h-[360px] md:h-[420px] lg:h-full bg-primary flex flex-col justify-start items-center sticky top-0 lg:static max-w-full">
+        <img className="block mb-[14px] lg:mb-8 mt-[75px] md:mt-[60px] lg:mt-[80px]  w-[100px] md:w-auto" src={euro} alt="..." />
+        <input id="number" type="number" value={23.50}  className="bg-primary	text-center text-white font-bold text-[80px] focus:outline-none max-w-full mb-[-15px] md:mb-0"/>  
         <div className="h-[1px] bg-blue w-5/6"></div>
-        <div className="text-blue text-[32px] mt-2">EUR</div>
+        <div className="text-blue text-[22px] md:text-[32px] mt-2">EUR</div>
       </div>
-      <div className="h-full p-10 overflow-y-auto">
+      <div className="h-full p-8 lg:p-10 overflow-y-auto">
       {[...Array(10)].map((x, i) =>
             <CurrencyCard key={i} />
           )}
